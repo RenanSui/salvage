23667d501264deb0fa3fbb926aeed4a22f424419
RenanSui <renanddtao@gmail.com>
2024-02-18 20:12:21 -0300

    organize pages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|_app| {
+            println!("Opening");
+
+            Command::new("explorer")
+                .arg("-C") // <- Specify the directory you'd like to open.
+                .arg("/Games")
+                .spawn()
+                .unwrap();
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Home() {
+
+  return (
+   <main>
+    <div className="bg-red-500 aspect-video">
+    </div>
+   </main>
+  );
+}
