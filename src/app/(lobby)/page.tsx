62bb856df20ae2941dca70f9546bab69188de6d1
RenanSui <renanddtao@gmail.com>
2024-08-06 18:15:48 -0300

    create lobby page and layout

---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,7 @@
+export default function Lobby() {
+  return (
+    <main>
+      <div className="hover:bg-custom-color-400 m-4 rounded-md transition-all border h-20 w-20"></div>
+    </main>
+  )
+}
