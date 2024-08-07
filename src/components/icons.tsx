918e7298f2cb9adb96ff81bc882d0b7e1e631ac8
RenanSui <renanddtao@gmail.com>
2024-08-07 09:43:50 -0300

    update icons properties

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -22,9 +22,9 @@ export const Icons = {
       <path
         d="M3 10H18"
         // stroke="#505154"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn(
           'stroke-[#505154] dark:stroke-[#ADAFB1] transition-all',
           middleStroke,
@@ -33,9 +33,9 @@ export const Icons = {
       <path
         d="M3 5H13M8 15H18"
         // stroke="#ADAFB1"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={cn(
           'stroke-[#ADAFB1] transition-all duration-500',
           sideStrokes,
@@ -55,9 +55,9 @@ export const Icons = {
     >
       <path
         d="M10.6429 1.41669L1.35718 9.84261M1.35718 1.41669L10.6429 9.84261"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
@@ -73,9 +73,9 @@ export const Icons = {
       <path
         d="M1.33337 1.2963H10.6667"
         stroke="#ADAFB1"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
