928abbdadf12ce1df2d3266f6d49a432dc09e6c4
RenanSui <renanddtao@gmail.com>
2024-08-07 15:15:15 -0300

    extend icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,12 @@
 import { cn } from '@/lib/utils'
+import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import { Trash2 } from 'lucide-react'
 
 export type IconProps = React.HTMLAttributes<SVGElement>
 
 export const Icons = {
+  dots: DotsHorizontalIcon,
+  delete: Trash2,
   logo: ({
     middleStroke,
     sideStrokes,
@@ -74,6 +78,24 @@ export const Icons = {
         d="M1.33337 1.2963H10.6667"
         stroke="#ADAFB1"
         strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  computerUpload: ({ className, ...props }: IconProps) => (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn('stroke-foreground', className)}
+      {...props}
+    >
+      <path
+        d="M10.5 1.5H2.5C2.10218 1.5 1.72064 1.65804 1.43934 1.93934C1.15804 2.22064 1 2.60218 1 3V14C1 14.3978 1.15804 14.7794 1.43934 15.0607C1.72064 15.342 2.10218 15.5 2.5 15.5H17.5C17.8978 15.5 18.2794 15.342 18.5607 15.0607C18.842 14.7794 19 14.3978 19 14V10.5M10 15.5V19.5M5 19.5H15M16.5 7.5V1.5M16.5 1.5L14 4M16.5 1.5L19 4"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
