75d7a4b71ba5c0038a1cc3c1cdfb74bf0c44afd9
RenanSui <renanddtao@gmail.com>
2024-08-07 11:50:18 -0300

    modify tailwind config to handle shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,63 @@
 import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
-const config: Config = {
-  darkMode: 'class', // data-theme="dark"
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+const config = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       colors: {
+        // custom
+        'custom-primary-500': '#08568C',
+        'custom-primary-200': '#6594B4',
+        'custom-dark-500': '#09090B',
+        'custom-dark-400': '#111113',
+        'custom-dark-300': '#27272A',
+        'custom-dark-200': '#383838',
+        'custom-dark-100': '#ADAFB1',
+        'custom-gray-500': '#505154',
+        'custom-gray-400': '#ADAFB1',
+        'custom-gray-300': '#DEDEDE',
+        'custom-gray-200': '#EBEBEA',
+        'custom-gray-100': '#F1F1F0',
+        // end of custom
         border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
-        'custom-color-500': 'hsl(var(--custom-color-100))',
-        'custom-color-400': 'hsl(var(--custom-color-400))',
-        'custom-color-300': 'hsl(var(--custom-color-300))',
-        'custom-color-200': 'hsl(var(--custom-color-200))',
-        'custom-color-100': 'hsl(var(--custom-color-100))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
         },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
           foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
         },
         popover: {
           DEFAULT: 'hsl(var(--popover))',
@@ -37,8 +73,29 @@ const config: Config = {
         mono: ['var(--font-geist-mono)', ...fontFamily.mono],
         heading: ['var(--font-heading)', ...fontFamily.sans],
       },
+
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config
+
 export default config
