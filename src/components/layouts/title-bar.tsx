4bcddf3b150d99d32cc4b9c481668c1501f148b7
RenanSui <renanddtao@gmail.com>
2024-08-07 10:47:26 -0300

    modify title bar to handle close and minimize

--- src/components/layouts/title-bar.tsx
+++ src/components/layouts/title-bar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { tauriWindow } from '@/lib/tauri'
 import { Icons } from '../icons'
 
 export default function TitleBar() {
   return (
-    <div className="bg-custom-color-100 dark:bg-custom-color-200 shadow-md flex items-center justify-between">
+    <header
+      className="bg-custom-color-300 dark:bg-custom-color-200 shadow-md flex items-center justify-between"
+      data-tauri-drag-region
+    >
       <button className="px-2 group">
         <Icons.logo
           className="transition-all"
@@ -11,13 +17,19 @@ export default function TitleBar() {
         />
       </button>
       <div className="flex items-center">
-        <button className="w-11 h-7 hover:bg-[#DEDEDE] dark:hover:bg-[#505154] flex items-center justify-center transition-all">
+        <button
+          className="w-11 h-7 hover:bg-[#505154] flex items-center justify-center transition-all cursor-default"
+          onClick={async () => (await tauriWindow())?.appWindow.minimize()}
+        >
           <Icons.minus />
         </button>
-        <button className="w-11 h-7 hover:bg-red-500 flex items-center justify-center group transition-all">
+        <button
+          className="w-11 h-7 hover:bg-red-500 flex items-center justify-center group transition-all cursor-default"
+          onClick={async () => (await tauriWindow())?.appWindow.close()}
+        >
           <Icons.cross className="group-hover:stroke-white" />
         </button>
       </div>
-    </div>
+    </header>
   )
 }
