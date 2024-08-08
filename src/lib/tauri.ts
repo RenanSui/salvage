b5222e1863ffd95109cffbc12118a8cea456fb09
RenanSui <renanddtao@gmail.com>
2024-08-08 11:46:13 -0300

    extend tauri lib: add invoke

--- src/lib/tauri.ts
+++ src/lib/tauri.ts
@@ -1,7 +1,21 @@
+import { InvokeArgs } from '@tauri-apps/api/tauri'
+
 export async function tauriWindow() {
   if (typeof window !== 'undefined') {
     const appWindow = await import('@tauri-apps/api/window')
     return appWindow
+  }
+  return null
+}
+
+export async function tauriInvoke<T>(
+  cmd: string,
+  args?: InvokeArgs | undefined,
+): Promise<T | null> {
+  if (typeof window !== 'undefined') {
+    const tauriAppsApi = await import('@tauri-apps/api')
+    const tauriInvoke = tauriAppsApi.invoke
+    return tauriInvoke(cmd, args)
   }
   return null
 }
