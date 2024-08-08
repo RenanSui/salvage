a461ffc5539c86d842e3a95b8ed87a99fc9ec35e
RenanSui <renanddtao@gmail.com>
2024-08-08 15:32:01 -0300

    add tauri command add_salvage_item

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod salvage;
+
 use rfd::FileDialog;
+use salvage::salvage as Salvage;
+use uuid::Uuid;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_file, get_folder])
+        .invoke_handler(tauri::generate_handler![
+            get_file,
+            get_folder,
+            add_salvage_item,
+        ])
         .run(tauri::generate_context!())
         .expect("# Error while running tauri application");
 }
@@ -31,3 +39,10 @@ fn get_folder() -> String {
         "".to_owned()
     }
 }
+
+#[tauri::command(rename_all = "snake_case")]
+fn add_salvage_item(mut salvage_item: Salvage::SalvageItem) {
+    salvage_item.id = Uuid::new_v4().to_string();
+    let _ = Salvage::add(salvage_item);
+}
+
