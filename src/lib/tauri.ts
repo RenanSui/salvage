e5774c9dc4ea3c36e8370fa5cdfdb8751c1c37d3
RenanSui <renanddtao@gmail.com>
2024-08-09 17:57:47 -0300

    better types for tauri invoke function

--- src/lib/tauri.ts
+++ src/lib/tauri.ts
@@ -1,5 +1,16 @@
 import { InvokeArgs } from '@tauri-apps/api/tauri'
 
+type tauriInvokeCmdArgs =
+  | 'get_salvage_items'
+  | 'add_salvage_item'
+  | 'get_file'
+  | 'get_folder'
+  | 'update_salvage_item_name'
+  | 'update_salvage_item_source'
+  | 'update_salvage_item_destination'
+  | 'update_salvage_item_exclusions'
+  | 'remove_salvage_item'
+
 export async function tauriWindow() {
   if (typeof window !== 'undefined') {
     const appWindow = await import('@tauri-apps/api/window')
@@ -9,7 +20,7 @@ export async function tauriWindow() {
 }
 
 export async function tauriInvoke<T>(
-  cmd: string,
+  cmd: tauriInvokeCmdArgs,
   args?: InvokeArgs | undefined,
 ): Promise<T | null> {
   if (typeof window !== 'undefined') {
