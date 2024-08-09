422131b5e1e087e0d893dad45c253e347b7c9371
RenanSui <renanddtao@gmail.com>
2024-08-09 17:59:42 -0300

    create react query provider

---
+++ src/components/providers/react-query-provider.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+export const ReactQueryProvider = ({
+  children,
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
