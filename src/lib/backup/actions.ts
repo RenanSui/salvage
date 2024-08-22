9d866876cbbea972ee7d2bd8ab52bfc9d777648e
RenanSui <renanddtao@gmail.com>
2024-08-22 11:14:11 -0300

    extend backup actions

--- src/lib/backup/actions.ts
+++ src/lib/backup/actions.ts
@@ -41,6 +41,29 @@ async function delete_backup(id: BackupSchema['id']) {
   return (await tauriInvoke<boolean>('delete_backup', { id })) || false
 }
 
+async function load_backups() {
+  return await tauriInvoke<BackupSchema[]>('load_backups')
+}
+
+async function start_watching() {
+  await tauriInvoke('start_watching')
+}
+async function stop_watching() {
+  await tauriInvoke('stop_watching')
+}
+async function restart_backups() {
+  await tauriInvoke('restart_backups')
+}
+async function start_individual_backup(id: BackupSchema['id']) {
+  await tauriInvoke('start_individual_backup', { id })
+}
+async function stop_individual_backup(id: BackupSchema['id']) {
+  await tauriInvoke('stop_individual_backup', { id })
+}
+async function restart_individual_backup(id: BackupSchema['id']) {
+  await tauriInvoke('restart_individual_backup', { id })
+}
+
 export const backupService = {
   select_file,
   select_folder,
@@ -52,4 +75,12 @@ export const backupService = {
   change_backup_destination,
   modify_backup_exclusions,
   delete_backup,
+  // Watcher
+  load_backups,
+  start_watching,
+  stop_watching,
+  restart_backups,
+  start_individual_backup,
+  stop_individual_backup,
+  restart_individual_backup,
 }
