3695d6a9e6bdc62e42d179b29fd7b093c32b882e
RenanSui <renanddtao@gmail.com>
2024-08-27 14:17:36 -0300

    modify backup dashboard skeleton

--- src/app/(lobby)/(backup)/_components/backup-dashboard-skeleton.tsx
+++ src/app/(lobby)/(backup)/_components/backup-dashboard-skeleton.tsx
@@ -1,21 +1,24 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { Skeleton } from '@/components/ui/skeleton'
 
 export function BackupDashboardSkeleton() {
   return (
-    <div className="w-full p-2 space-y-4">
-      <Skeleton className="h-8 w-36" />
-      <div className="space-y-1.5">
-        <Skeleton className="h-5 w-16" />
-        <Skeleton className="h-20 w-full" />
+    <ScrollArea className="h-[calc(100vh-78px)] w-full">
+      <div className="w-full p-2 space-y-4">
+        <Skeleton className="h-8 w-36" />
+        <div className="space-y-1.5">
+          <Skeleton className="h-5 w-16" />
+          <Skeleton className="h-20 w-full" />
+        </div>
+        <div className="space-y-1.5">
+          <Skeleton className="h-5 w-16" />
+          <Skeleton className="h-20 w-full" />
+          <Skeleton className="h-20 w-full" />
+          <Skeleton className="h-20 w-full" />
+          <Skeleton className="h-20 w-full" />
+          <Skeleton className="h-20 w-full" />
+        </div>
       </div>
-      <div className="space-y-1.5">
-        <Skeleton className="h-5 w-16" />
-        <Skeleton className="h-20 w-full" />
-        <Skeleton className="h-20 w-full" />
-        <Skeleton className="h-20 w-full" />
-        <Skeleton className="h-20 w-full" />
-        <Skeleton className="h-20 w-full" />
-      </div>
-    </div>
+    </ScrollArea>
   )
 }
