62e611b990e5f23bda7f7069b2463e49be3f58ed
RenanSui <renanddtao@gmail.com>
2024-10-23 20:45:53 -0300

    feat: add button card

--- src/app/(dashboard)/_components/backup-item.tsx
+++ src/app/(dashboard)/_components/backup-item.tsx
@@ -1,7 +1,6 @@
 import { Icons } from '@/components/icons'
+import { ButtonCard } from '@/components/ui/button-card'
 import { type BackupSchema } from '@/types'
-import { ChevronRightIcon } from '@radix-ui/react-icons'
-import Link from 'next/link'
 
 type BackupItemProps = { backup: BackupSchema }
 
@@ -9,18 +8,14 @@ export function BackupItem({ backup }: BackupItemProps) {
   const Icon = backup.is_file ? Icons.file : Icons.folder
 
   return (
-    <Link
-      href={`/backup/${backup.id}`}
-      className="group relative flex cursor-default items-center gap-2 rounded-md border border-transparent px-4 py-3 transition-colors duration-150 ease-in-out hover:border-border hover:bg-app-card"
-    >
-      <div className="flex size-8 items-center justify-center rounded-lg bg-app-background transition-colors duration-150 ease-in-out hover:!bg-foreground/20 group-hover:bg-foreground/10">
+    <ButtonCard.Link variant="ghost" href={`/backup/${backup.id}`}>
+      <ButtonCard.Icon variant="ghost">
         <Icon className="size-4" />
-      </div>
-      <div className="space-y-1">
-        <p className="text-sm font-medium">{backup.name}</p>
-        <p className="max-w-[400px] truncate text-xs text-stone-400">{backup.source}</p>
-      </div>
-      <ChevronRightIcon className="ml-auto size-4" />
-    </Link>
+      </ButtonCard.Icon>
+      <ButtonCard.Header>
+        <ButtonCard.Title>{backup.name}</ButtonCard.Title>
+        <ButtonCard.Description>{backup.source}</ButtonCard.Description>
+      </ButtonCard.Header>
+    </ButtonCard.Link>
   )
 }
