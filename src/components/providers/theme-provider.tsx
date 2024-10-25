a1f8383377ca84ee70ce43dbd52615c39b115d1e
RenanSui <renanddtao@gmail.com>
2024-10-25 12:30:05 -0300

    fix: hydration

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { useMounted } from '@/hooks/use-mounted'
 import { ThemeProvider } from 'next-themes'
 import { type HTMLAttributes } from 'react'
 
 export function NextThemesProvider({ children }: HTMLAttributes<HTMLDivElement>) {
+  const mounted = useMounted()
+  if (!mounted) return <></>
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       {children}
